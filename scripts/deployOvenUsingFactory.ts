b75982265e519daeaa31012355f84b63b52bcbf6
stevenkcolin <steven.k.colin@gmail.com>
2020-11-26 16:51:12 +0800

    submit

--- scripts/deployOvenUsingFactory.ts
+++ scripts/deployOvenUsingFactory.ts
@@ -1,8 +1,8 @@
 async function deployOvenUsingFactory() {
     // use env files for this?
-    const OvenFactoryAddress = "0x4a1d8455A2fEF2C922865A9988a2d617FEad4fF9"
-    const Pie = "0x4a1d8455A2fEF2C922865A9988a2d617FEad4fF9"
-    const Recipe = "0x4a1d8455A2fEF2C922865A9988a2d617FEad4fF9"
+    const OvenFactoryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    const Pie = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    const Recipe = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
     const OvenFactory = await ethers.getContractAt("OvenFactoryContract", OvenFactoryAddress);
     const tx = await OvenFactory.CreateOven(Pie, Recipe);
@@ -10,14 +10,14 @@ async function deployOvenUsingFactory() {
     const OvenAddress = txData.events[0].args.Oven
 
     console.log("Oven deployed to:", OvenAddress);
-    console.log("Please RUN:")
-    console.log(
-        "npx hardhat verify --network {network}",
-        OvenAddress,
-        OvenFactoryAddress,
-        Pie,
-        Recipe,
-    )
+    // console.log("Please RUN:")
+    // console.log(
+    //     "npx hardhat verify --network {network}",
+    //     OvenAddress,
+    //     OvenFactoryAddress,
+    //     Pie,
+    //     Recipe,
+    // )
   }
 
 deployOvenUsingFactory()

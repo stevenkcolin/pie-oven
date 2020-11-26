01c00b6452314845d2957e51279d06ce213fc17d
stevenkcolin <steven.k.colin@gmail.com>
2020-11-26 16:24:09 +0800

    submit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,10 +50,6 @@ export default {
   networks: {
     fork : {
       url: `http://127.0.0.1:8545/`,
-      gasPrice: 42000000000,
-      accounts: [
-        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-      ],
       timeout: 2147483647
     },
     mainnet: {
--- scripts/mainnettest/interestingrecipe.ts
+++ scripts/mainnettest/interestingrecipe.ts
@@ -54,20 +54,20 @@ async function mainnet2() {
     // await recipe.setBPool("0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", "0x7c90a3cd7ec80dd2f633ed562480abbeed3be546")
     // await recipe.setBPool("0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F", "0xe5ac9548275787cd86df2350248614afab0088ee")
 
-    console.log("calcToPie")
-    let x = await recipe.callStatic.calcToPie(
-        "0xb8d379c83a10b073565bd486ea87147e7117b025",
-        ethers.utils.parseEther("0.5")
-    )
-    console.error(x.toString())
+    // console.log("calcToPie")
+    // let x = await recipe.callStatic.calcToPie(
+    //     "0xb8d379c83a10b073565bd486ea87147e7117b025",
+    //     ethers.utils.parseEther("0.5")
+    // )
+    // console.error(x.toString())
 
-    console.log("toPie")
-    x = await recipe.toPie(
-        "0xb8d379c83a10b073565bd486ea87147e7117b025",
-        ethers.utils.parseEther("0.5"),
-        {"value": ethers.utils.parseEther("1")}
-    )
-    console.error(x)
+    // console.log("toPie")
+    // x = await recipe.toPie(
+    //     "0xb8d379c83a10b073565bd486ea87147e7117b025",
+    //     ethers.utils.parseEther("0.5"),
+    //     {"value": ethers.utils.parseEther("1")}
+    // )
+    // console.error(x)
 
 
 }

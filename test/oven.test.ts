76c7210819b0945863b9bb6b8edbec830a7efe82
stevenkcolin <steven.k.colin@gmail.com>
2020-11-26 18:50:49 +0800

    Update oven.test.ts

--- test/oven.test.ts
+++ test/oven.test.ts
@@ -276,109 +276,129 @@ describe("Test Deployment flow", function () {
   })
 });
 
-// describe("Test baking", function () {
-//   let pool : any;
-//   let recipe : any;
-//   let owner : any;
-//   let user1 : any;
-//   let user2 : any;
-//   let user3 : any;
-//   let oven : any;
+describe("Test baking", function () {
+  let pool : any;
+  let recipe : any;
+  let owner : any;
+  let user1 : any;
+  let user2 : any;
+  let user3 : any;
+  let oven : any;
 
-//   beforeEach(async function () {
-//     [owner, user1, user2, user3] = await ethers.getSigners();
-//     const TestPieRecipe = await ethers.getContractFactory(
-//       "TestPieRecipe"
-//     );
-//     recipe = await TestPieRecipe.deploy();
-//     await recipe.deployed();
+  beforeEach(async function () {
+    [owner, user1, user2, user3] = await ethers.getSigners();
+    const TestPieRecipe = await ethers.getContractFactory(
+      "TestPieRecipe"
+    );
+    recipe = await TestPieRecipe.deploy();
+    await recipe.deployed();
 
-//     const TestPie = await ethers.getContractFactory(
-//       "TestPie"
-//     );
-//     pool = await TestPie.deploy(parseEther("10000000000"), recipe.address);
-//     await pool.deployed();
+    const TestPie = await ethers.getContractFactory(
+      "TestPie"
+    );
+    pool = await TestPie.deploy(parseEther("10000000000"), recipe.address);
+    await pool.deployed();
 
-//     const Oven = await ethers.getContractFactory("Oven");
-//     oven = await Oven.deploy(owner.getAddress(), pool.address, recipe.address);
-//     await oven.deployed();
+    const Oven = await ethers.getContractFactory("Oven");
+    oven = await Oven.deploy(owner.getAddress(), pool.address, recipe.address);
+    await oven.deployed();
 
-//     await oven.setCap(parseEther("1000"));
-//     await oven.connect(user1).deposit({ value: parseEther("100") })
-//     await oven.connect(user2).deposit({ value: parseEther("100") })
-//     await oven.connect(user3).deposit({ value: parseEther("100") })
-//   });
+    await oven.setCap(parseEther("1000"));
+    await oven.connect(user1).deposit({ value: parseEther("100") })
+    await oven.connect(user2).deposit({ value: parseEther("100") })
+    await oven.connect(user3).deposit({ value: parseEther("100") })
+  });
 
-//   it("Non controller", async function () {
-//       await expect (
-//         oven.connect(user1).bake([owner.getAddress()], parseEther("1"), parseEther("2"))
-//       ).to.be.revertedWith("NOT_CONTROLLER")
-//   })
-//   it("Price too much", async function () {
-//     await recipe.testSetCalcToPieAmount(parseEther("50"))
-//     await expect (
-//       oven.bake([owner.getAddress()], parseEther("1"), parseEther("25"))
-//     ).to.be.revertedWith("PRICE_ERROR")
-//   })
-//   it("Total insufficient funds", async function () {
-//     // price needed
-//     await recipe.testSetCalcToPieAmount(parseEther("310"))
+  // it("Non controller", async function () {
+  //     await expect (
+  //       oven.connect(user1).bake([owner.getAddress()], parseEther("1"), parseEther("2"))
+  //     ).to.be.revertedWith("NOT_CONTROLLER")
+  // })
+  // it("Price too much", async function () {
+  //   await recipe.testSetCalcToPieAmount(parseEther("50"))
+  //   await expect (
+  //     oven.bake([owner.getAddress()], parseEther("1"), parseEther("25"))
+  //   ).to.be.revertedWith("PRICE_ERROR")
+  // })
+  // it("Total insufficient funds", async function () {
+  //   // price needed
+  //   await recipe.testSetCalcToPieAmount(parseEther("310"))
 
-//     await expect (
-//       oven.bake(
-//         [user1.getAddress(), user2.getAddress(), user3.getAddress()],
-//         parseEther("10"),
-//         parseEther("350")
-//       )
-//     ).to.be.revertedWith("INSUFFICIENT_FUNDS")
-//   })
-//   it("Success (exact)", async function () {
-//     // price needed
-//     await recipe.testSetCalcToPieAmount(parseEther("300"))
-//     const users = [user1, user2, user3];
-//     for(const user of users) {
-//       await expect(await oven.ethBalanceOf(user.getAddress())).to.be.eq(parseEther("100"));
-//       await expect(await oven.outputBalanceOf(user.getAddress())).to.be.eq(parseEther("0"));
-//     }
+  //   await expect (
+  //     oven.bake(
+  //       [user1.getAddress(), user2.getAddress(), user3.getAddress()],
+  //       parseEther("10"),
+  //       parseEther("350")
+  //     )
+  //   ).to.be.revertedWith("INSUFFICIENT_FUNDS")
+  // })
+  // it("Success (exact)", async function () {
+  //   // price needed
+  //   await recipe.testSetCalcToPieAmount(parseEther("300"))
+  //   const users = [user1, user2, user3];
+  //   for(const user of users) {
+  //     // await expect(await oven.ethBalanceOf(user.getAddress())).to.be.eq(parseEther("100"));
+  //     const value = await oven.ethBalanceOf(user.getAddress());
+  //     console.log("value is: ",value.toString());
+  //     // await expect(await oven.outputBalanceOf(user.getAddress())).to.be.eq(parseEther("0"));
+  //   }
 
-//     await oven.bake(
-//       [user1.getAddress(), user2.getAddress(), user3.getAddress()],
-//       parseEther("30"),
-//       parseEther("300")
-//     )
+  //   console.log("---------------------------------------------------------------------");
+  //   // await oven.bake(
+  //   //   [user1.getAddress(), user2.getAddress(), user3.getAddress()],
+  //   //   parseEther("45"),
+  //   //   parseEther("300")
+  //   // );
 
-//     for(const user of users) {
-//       await expect(await oven.ethBalanceOf(user.getAddress())).to.be.eq(parseEther("0"));
-//       await expect(await oven.outputBalanceOf(user.getAddress())).to.be.eq(parseEther("10"));
-//     };
+  //   await oven.bake(
+  //     [user1.getAddress(), user2.getAddress(),user3.getAddress()],
+  //     parseEther("30"),
+  //     parseEther("300")
+  //   )
 
-//   })
-//   it("Success (too much)", async function () {
-//     // price needed
-//     await recipe.testSetCalcToPieAmount(parseEther("250"))
-//     const users = [user1, user2, user3];
-//     for(const user of users) {
-//       await expect(await oven.ethBalanceOf(user.getAddress())).to.be.eq(parseEther("100"));
-//       await expect(await oven.outputBalanceOf(user.getAddress())).to.be.eq(parseEther("0"));
-//     };
+  //   for(const user of users) {
+  //     // await expect(await oven.ethBalanceOf(user.getAddress())).to.be.eq(parseEther("0"));
+  //     // await expect(await oven.outputBalanceOf(user.getAddress())).to.be.eq(parseEther("10"));
+  //     const value = await oven.outputBalanceOf(user.getAddress());
+  //     console.log("value is: ",value.toString());
+  //   };
+  // })
+  // it("Success (too much)", async function () {
+  //   // price needed
+  //   await recipe.testSetCalcToPieAmount(parseEther("150"))
+  //   const users = [user1, user2, user3];
+  //   for(const user of users) {
+  //     await expect(await oven.ethBalanceOf(user.getAddress())).to.be.eq(parseEther("100"));
+  //     await expect(await oven.outputBalanceOf(user.getAddress())).to.be.eq(parseEther("0"));
+  //   };
 
-//     await oven.bake(
-//       [user1.getAddress(), user2.getAddress(), user3.getAddress()],
-//       parseEther("30"),
-//       parseEther("300")
-//     )
+  //   await oven.bake(
+  //     [user1.getAddress(), user2.getAddress(), user3.getAddress()],
+  //     parseEther("80"),
+  //     parseEther("300")
+  //   )
 
-//     await expect(await oven.ethBalanceOf(user1.getAddress())).to.be.eq(parseEther("0"));
-//     await expect(await oven.outputBalanceOf(user1.getAddress())).to.be.eq(parseEther("12"));
+  //   for(const user of users) {
+  //     const ethBalanceOf = await oven.ethBalanceOf(user.getAddress());
+  //     console.log("ethBalanceOf is: ",ethBalanceOf.toString());
+  //   };
 
-//     await expect(await oven.ethBalanceOf(user2.getAddress())).to.be.eq(parseEther("0"));
-//     await expect(await oven.outputBalanceOf(user2.getAddress())).to.be.eq(parseEther("12"));
+  //   for(const user of users) {
+  //     const outputBalanceOf = await oven.outputBalanceOf(user.getAddress());
+  //     console.log("outputBalanceOf is: ",outputBalanceOf.toString());
+  //   };
 
-//     await expect(await oven.ethBalanceOf(user3.getAddress())).to.be.eq(parseEther("50"));
-//     await expect(await oven.outputBalanceOf(user3.getAddress())).to.be.eq(parseEther("6"));
-//   })
+    // await expect(await oven.ethBalanceOf(user1.getAddress())).to.be.eq(parseEther("0"));
+    // await expect(await oven.outputBalanceOf(user1.getAddress())).to.be.eq(parseEther("12"));
 
-// })
+    // await expect(await oven.ethBalanceOf(user2.getAddress())).to.be.eq(parseEther("0"));
+    // await expect(await oven.outputBalanceOf(user2.getAddress())).to.be.eq(parseEther("12"));
+
+    // await expect(await oven.ethBalanceOf(user3.getAddress())).to.be.eq(parseEther("50"));
+    // await expect(await oven.outputBalanceOf(user3.getAddress())).to.be.eq(parseEther("6"));
+  })
+
+})
 
 // describe("Test deposit/withdraw eth + cap", function () {
 //   let pool : any;
